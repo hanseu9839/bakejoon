3f99f736c82b6407af57d14584f775d3ec3f5b31
hanseu9839 <hanseu9839@gmail.com>
2022-05-01 22:47:34 +0900

    resolve

--- Level10/Baekjoon17478/Baekjoon17478.java
+++ Level10/Baekjoon17478/Baekjoon17478.java
@@ -24,7 +24,9 @@ public class Baekjoon17478 {
         System.out.println(tap+" \"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
         System.out.println(tap+"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
         System.out.println(tap+"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾와서 물었어.\" ");
+        System.out.println(tap+"라고 답변하였지.");
         sentenceTap+="____";
+        
         sentence(n-1);
 
     }
