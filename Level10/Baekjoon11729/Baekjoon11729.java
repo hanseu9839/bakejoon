ee7845229b661778cf46dd2449e2343bb34304b7
hanseu9839 <hanseu9839@gmail.com>
2022-05-04 19:11:53 +0900

    Hanoi-Top

--- Level10/Baekjoon11729/Baekjoon11729.java
+++ Level10/Baekjoon11729/Baekjoon11729.java
@@ -11,15 +11,15 @@ public class Baekjoon11729 {
         sb.append((int)(Math.pow(2,N)-1)).append('\n');
         Hanoi(N,1,2,3);
         System.out.println(sb);
-    }
+    }                              //  123
     public static void Hanoi(int N,int start, int mid, int to){
         if(N==1){
-            sb.append(start+" "+to+"\n");//1 3
+            sb.append(start+" "+to+"\n"); //1 2
             return;
         }
-
-        Hanoi(N-1,start,to,mid);//
-        sb.append(start+ " "+to+"\n");// 
-        Hanoi(N-1,mid,start,to);
-    }
+                                
+        Hanoi(N-1,start,to,mid);//3 1 3 2
+        sb.append(start+ " "+to+"\n");//2 1 3 
+        Hanoi(N-1,mid,start,to);// 1 
+    }//(1 3) (2 3) 
 }
