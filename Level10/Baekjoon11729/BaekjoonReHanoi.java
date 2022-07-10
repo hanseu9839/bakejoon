c0d5b77990edf6b2de6b420c06cfa298e6f73544
hanseu9839 <hanseu9839@gmail.com>
2022-07-10 22:32:43 +0900

    reProbelm solve Hanoi_Top_recuresive_Algorithm
    d

--- Level10/Baekjoon11729/BaekjoonReHanoi.java
+++ Level10/Baekjoon11729/BaekjoonReHanoi.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class BaekjoonReHanoi {
     public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            int N = scan.nextInt();
-            sb.append((int)(Math.pow(2,N)-1)).append("\n");
+            Scanner in = new Scanner(System.in);
+            int N = in.nextInt();
+            sb.append((int)(Math.pow(2,N)-1) + "\n");
             Hanoi(N,1,2,3);
-            System.out.println(sb);
+            System.out.print(sb);
     }
     public static void Hanoi(int N,int start,int mid,int from){
-        if(N==1){
-            sb.append(start+" "+from+"\n");
-            return;
-        }
-        Hanoi(N-1,start,from,mid);
-        sb.append(start+ " "+ from+"\n");
-        Hanoi(N-1,mid,start,from);
-
+       if(N==1){
+           sb.append(start+" "+from +"\n");
+           return;
+       }
+       Hanoi(N-1,start,from,mid);
+       sb.append(start+" "+from+"\n");
+       Hanoi(N-1,mid,start,from);
 
     }
 }
