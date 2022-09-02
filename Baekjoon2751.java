d5eafbade6a652ab690faa21f46024cf074020a0
hanseu9839 <hanseu9839@gmail.com>
2022-09-02 14:05:35 +0900

    Baekjoon 2751 time over error Merge Sort

--- Baekjoon2751.java
+++ Baekjoon2751.java
@@ -2,28 +2,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Baekjoon2751 {
-    static int[] arr;
-    static void solution(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
+    
+    static int[] sorted;
+    static void sort(int []arr){
+        sorted = new int[arr.length];
+        mergeSort(arr,0,arr.length-1);
+    }
+    static void mergeSort(int []arr ,int left, int right){
+        int mid = (left + right) /2;
+        if(left==right) return;
+
+        mergeSort(arr,left,mid);
+        mergeSort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+    static void merge(int[] arr,int left, int mid ,int right){
+        int l = left;
+        int r = mid+1;
+        int idx = left;
+        while(r<=right && l <= mid){
+            if(arr[l] <= arr[r]) {
+                sorted[idx]=arr[l];
+                l++;
+                idx++;
+            }else{
+                sorted[idx]=arr[r];
+                r++;
+                idx++;
             }
+        }
+        if(l>mid){
+            while(r<=right){
+                sorted[idx]=arr[r];
+                r++;
+                idx++;
+            }
+        }else{
+            while(l<=mid){
+                sorted[idx]=arr[l];
+                l++;
+                idx++;
+            }
+        }
+        for(int i=left;i<=right;i++){
+            arr[i]=sorted[i];
         }
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        int []arr = new int[n];
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        solution(n);
-        for(int x : arr){
-            System.out.println(x);
-        }
+        sort(arr);
+        for(int x : sorted) System.out.println(x);
     }
 }
